829b15d0f9f59c8079ba6bcc192c0ceffcd4ae06
Alexandre <>
2021-05-21 23:40:53 +0000

    Init project

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,17 @@
 def arithmetic_arranger(problems):
+  # Get problems
+
+  # Test if data is OK else error
+
+  # Count number of data
+
+  # assign variable for each in lines and x&y
+
+  # Foreach x&y, take the largest number, add 2 (space and symbol)
+  # We now have width of each calcul
+
+  # Line 1 = (maxlen+2  - len(x1)) of spaces concat with (if there is x2) 4 spaces then same for x2... concat with \n
+  #Then line 2
 
 
-    return arranged_problems
+  return arranged_problems
